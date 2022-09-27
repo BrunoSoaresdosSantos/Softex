334651b58bc78eac8d9537ac90ac5ea393a75786
Bruno Soares dos santos <83316195+BrunoSoaresdosSantos@users.noreply.github.com>
2022-09-27 20:47:02 -0300

    Add files via upload

---
+++ Exercicio7 Softex.py
@@ -0,0 +1,17 @@
+from statistics import median
+import string
+nome = "Bruno Soares dos Santos"
+nota1 = 8.5 
+nota2 = 6.5
+media = (nota1 + nota2)/2
+falta = 2
+
+if (media < 7 or falta > 3):
+    if(media < 7):
+        print("Aluno ",nome," reprovado por nota com a note de: ",media)
+    else:
+        print("Aluno ",nome," reprovado por falta com ",falta," faltas.")
+else:
+    print("aluno está aprovado com a media de: ",media)
+
+
