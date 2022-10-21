17ce462d4b8ba937711ec51dfbb6cab97b20a905
Bruno Soares dos santos <83316195+BrunoSoaresdosSantos@users.noreply.github.com>
2022-10-21 14:10:50 -0300

    Add files via upload

---
+++ Exercicio24Softex/aluguel/src/App.java
@@ -0,0 +1,14 @@
+import br.com.veiculosalugaveis.pessoa.*;
+import br.com.veiculosalugaveis.veiculos.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Carro carro = new Carro();
+
+        Moto moto = new Moto();
+
+        Cliente cliente = new Cliente();
+
+        Funcionario funcionario = new Funcionario();
+    }
+}
